1f745908caaef0a94cbf41dfd765fdf34638d8b9
yushp <yshpak-transcenda@spoton.com>
2023-03-13 22:02:29 +0200

    Created container deploy script

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationConfig = require('webpack/lib/container/ModuleFederationPlugin');
 
 const packageJson = require('../package.json');
@@ -14,9 +13,6 @@ const devConfig = {
         },
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        }),
         new ModuleFederationConfig({
             name: 'marketing',
             filename: 'remoteEntry.js',
